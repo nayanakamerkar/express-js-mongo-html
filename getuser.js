79511921aa2981ae1ca310d04e4a1b58823fb789
nayanakamerkar <40195293+nayanakamerkar@users.noreply.github.com>
2018-06-22 18:06:44 +0530

    api

--- getuser.js
+++ getuser.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+var mongodb=require('mongodb');
+var bodyParser = require('body-parser');
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
@@ -9,36 +10,32 @@ MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("Assignment");
 
-  app.use(function (req, res, next) {
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function (req, res, next) {
 	    // Website you wish to allow to connect
-	    if(req.headers.origin)
-	    {
-	    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-	    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-	    res.setHeader('Access-Control-Allow-Credentials', true);
+	if(req.headers.origin) {
+	   	res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+		res.setHeader('Access-Control-Allow-Credentials', true);
 	    }
 	    next();
-  });
-  
+    });
 
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
 	res.send("Hello World!");
-  })
+})
 
-  app.get('/users', (req, res) => {
-  	console.log("req.query", req.query);
-  	console.log("req.body", req.body);
-
-
-  	dbo.collection("Users").find({}).toArray(function(err, result) {
+app.get('/users', (req, res) => {
+	dbo.collection("Users").find({}).toArray(function(err, result) {
 	    if (err) throw err;
 	    console.log(result);
 	    var myjson=JSON.stringify(result);
 	    res.send(myjson);
 	});
-  });
-
+});
 
 app.get('/city', (req, res) => {
   	dbo.collection("City").find({}).toArray(function(err, result) {
@@ -47,31 +44,95 @@ app.get('/city', (req, res) => {
 	    var myjson2=JSON.stringify(result);
 	    res.send(myjson2);
 	});
-  });
+});
 
-app.get('/state', (req, res) => {
+app.get('/states', (req, res) => {
   	dbo.collection("State").find({}).toArray(function(err, result) {
 	    if (err) throw err;
 	    console.log(result);
 	    var myjson1=JSON.stringify(result);
 	    res.send(myjson1);
 	});
-  });
-app.get('/usercity',(req,res)=>{
-	dbo.collection("Users".find().limit(c).toArray(function(err,result){
-		if(err) throw err;
-		console.log(result);
-	});
 });
 
+app.get('/count', (req, res) => {
+	//console.log("req.query", req);
+
+  	console.log("req.body", res);
+  dbo.collection("Users").find().limit(1).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    var myjson3=JSON.stringify(result);
+	    res.send(myjson3);
+  });
+});
+
+app.post('/cityselect',(req,res)=> {
+    var body = req.body;
+
+    console.log("body =", body);
+    if(!body.cname){
+      res.send(JSON.stringify({ mess: "cname parameter missing"}));
+    }
+    else{
+      dbo.collection("Users").find({city: body.cname }).toArray(function(err,result){
+        if(err) throw err;
+        console.log(result);
+        var myjson4=JSON.stringify(result);
+          res.send(myjson4);
+      });
+    }
+  });
+
+
+app.post('/insert', function (req, res) {
+  var body1 = req.body;
+    // for safety reasons
+    console.log("body =", body1);
+    if(!body1.name|| !body1.mobile || !body1.address || !body1.city || !body1.state || !body1.gender){
+      res.send(JSON.stringify({ mess: " parameter missing"}));
+    }   
+    else{
+      dbo.collection("Users").insertOne(body1,function(err,result){
+        if(err) throw err;
+        console.log(result);
+        var myjson4=JSON.stringify(result);
+          res.send(myjson4);
+      });
+     }
+  });
+
+app.post('/delete', function (req, res) {
+  var body2 = req.body;
+    // for safety reasons
+    console.log("body =", body2);
+    if(!body2._id){
+      res.send(JSON.stringify({ mess: " parameter missing"}));
+    }   
+    else{
+      console.log("body =", body2._id);
+      dbo.collection("Users").deleteOne({_id: new mongodb.ObjectID(body2._id)},function(err,result){
+        if(err) throw err;
+        console.log("document deleted");
+      });
+     }
+  });
+
+app.post('/update', function (req, res) {
+  var body1 = req.body;
+  
+  var body2 = body1._id;
+  console.log(body2);
+  delete body1._id;
+  newquery = { $set: body1 }
+  console.log(new mongodb.ObjectID(body2));
+  dbo.collection('Users').updateOne({ _id: new mongodb.ObjectID(body2)},newquery,function(err,result){
+    if(err) throw err;
+    console.log("document updated");
+    });
+});
 
 
 
   app.listen(3000, () => console.log('Example app listening on port 3000!'))
 });
-
-/*var server = app.listen(3000, function() {}); 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/name ', (req, res) => res.send('Hello World!'))*/
-
-
